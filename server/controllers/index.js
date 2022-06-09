b6520c9fa9a48a62488dc12ed02e7f9f6c8ec93f
Donna Szeto <szetodonna2@gmail.com>
2022-06-09 16:17:39 -0700

    completed query for /qa/questions  Please enter the commit message for your changes. Lines starting

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,17 +4,33 @@ module.exports = {
 
   getQuestions: function(req, res) {
     console.log('req.param.product_id',req.query.product_id )
-    let queryStr = `select id, question_body, question_date, asker_name, question_helpfulness, reported from questions where product_id = ${req.query.product_id};`
-    pool.query(queryStr)
+    let queryStr = `select id, question_body, question_date, asker_name, question_helpfulness, reported,
+    (select json_object_agg(
+      answers.id, json_build_object(
+        'id', id,
+        'answer_body',answer_body,
+        'photos',(select json_agg(
+          json_build_object(
+            'id', id,
+            'url', pic_url))
+            as photos from photos
+            where answer_id=answers.id)))
+            as answers from answers
+            where question_id=questions.id)
+            from questions where product_id = $1;`
+    pool.query(queryStr,[req.query.product_id])
     .then((data) => {
       let result = {
         "product_id": req.query.product_id,
         "results": data.rows
       }
+      // let queryStr = ``
+      // pool.query(queryStr)
       res.status(200).send(result);
     })
     .catch((err) => {
-      res.status(500).send('could not access data')
+      console.log('error', err);
+      res.status(500).send(err)
     })
   }
 }
